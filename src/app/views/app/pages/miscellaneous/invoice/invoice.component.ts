d471afc0cd075e2ef665c42384c98f05546f0265
HRITHIK <hrithikrtiwari@gmail.com>
2020-06-17 13:57:21 +0530

    ,,

--- src/app/views/app/pages/miscellaneous/invoice/invoice.component.ts
+++ src/app/views/app/pages/miscellaneous/invoice/invoice.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { timeRequired } from './custom.validators';
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html'
@@ -24,9 +26,23 @@ export class InvoiceComponent implements OnInit {
   ShippingCharges = '---';
   Total = '----';
   TotalAmount = '---';
+
+  formExternalComponents: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.formExternalComponents = new FormGroup({
+      name: new FormControl(null, [Validators.required, Validators.minLength(2), Validators.pattern('^[A-Za-z]+$')]),
+      ngSelect: new FormControl(null, [Validators.required]),
+      basicDate: new FormControl(null, [Validators.required]),
+      basicTime: new FormControl(null, [timeRequired()]),
+      switch: new FormControl(null, [Validators.requiredTrue])
+    });
+  }
+
+
+  onSubmit() {
+    console.log(this.formExternalComponents);
   }
 
 }
